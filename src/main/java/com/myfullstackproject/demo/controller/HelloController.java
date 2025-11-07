27aec0bb9c2211f1187e028fe0aea6c020ac7e80
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-11-07 09:15:17 -0300

    feat: implement HelloService class with basic business logic

--- src/main/java/com/myfullstackproject/demo/controller/HelloController.java
+++ src/main/java/com/myfullstackproject/demo/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.myfullstackproject.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
+	
 }
--- src/main/java/com/myfullstackproject/demo/service/HelloService.java
+++ src/main/java/com/myfullstackproject/demo/service/HelloService.java
@@ -1,5 +1,11 @@
 package com.myfullstackproject.demo.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HelloService {
 
+	public String getHelloMessage() {
+		return "Hello Full Stack Developer! This message came from the Service Layer via Dependency Injection.";
+	}
 }

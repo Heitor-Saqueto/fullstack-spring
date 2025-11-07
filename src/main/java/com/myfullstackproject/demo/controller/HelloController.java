aab81da908ffce2212c05f8b524970e09cc04998
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-11-07 09:20:42 -0300

    feat: implement HelloController and inject HelloService

--- src/main/java/com/myfullstackproject/demo/controller/HelloController.java
+++ src/main/java/com/myfullstackproject/demo/controller/HelloController.java
@@ -1,9 +1,22 @@
 package com.myfullstackproject.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.myfullstackproject.demo.service.HelloService;
 
 @RestController
 public class HelloController {
 
+	private final HelloService helloService;
 	
+	//Constructor Injection 
+	public HelloController(HelloService helloService) {
+		this.helloService = helloService;
+	}
+	
+	@GetMapping
+	public String sayHello() {
+		return helloService.getHelloMessage();
+	}
 }

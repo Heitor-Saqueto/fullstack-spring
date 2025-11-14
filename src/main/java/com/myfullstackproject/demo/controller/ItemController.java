989c450c770ba16490d2f2c1bb6a9f18f4f46970
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-11-14 10:51:48 -0300

    feat: implement update endpoint to finalize CRUD functionality

--- src/main/java/com/myfullstackproject/demo/controller/ItemController.java
+++ src/main/java/com/myfullstackproject/demo/controller/ItemController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +41,28 @@ public class ItemController {
 		List<Item> items = itemService.findAll();
 		//Return status 200 ok and list all items
 		return ResponseEntity.ok(items);
+	}
+	
+	//New Get by id
+	@GetMapping("/{id}")
+	public ResponseEntity<Item> findById(@PathVariable Long id){
+		Item item = itemService.findById(id);
+		return ResponseEntity.ok(item); //It returns 200 OK 
+	} 
+	
+	//Update by id
+	@PutMapping("/{id}")
+	public ResponseEntity<Item> updateItem(@PathVariable Long id, @RequestBody Item updatedItem){
+		
+		Item result = itemService.update(id, updatedItem);
+		//returns 200 OK with updated Item
+		return ResponseEntity.ok(result);
+	}
+	//Delete by Id
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id){
+		itemService.deleteById(id);
+		// return 204 no content.
+		return ResponseEntity.noContent().build();
 	}
 }
